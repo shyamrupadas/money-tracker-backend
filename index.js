7a3570c00cdd13a14cb30d715bc60527c6424e25
Sergei Maksimov.com <s.mksimov@gmail.com>
2021-10-23 23:15:52 +0300

    added post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Card from './Card.js';
 
 const PORT = 5000;
 const DB_URL = 'mongodb+srv://user:user@cluster0.ivmut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
@@ -7,14 +8,24 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  res.status(200).json('Server online!');
+app.post('/', async (req, res) => {
+  try {
+    const { name, sum, actualDate } = req.body;
+    const card = await Card.create({ name, sum, actualDate });
+    res.json(card);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+
+
 });
 
 async function startApp() {
   try {
     await mongoose.connect(DB_URL);
-    app.listen(PORT, () => { console.log('you server is started on port' + PORT) });
+    app.listen(PORT, () => {
+      console.log('Your server is started on port' + PORT)
+    });
   } catch (e) {
     console.log(e);
   }
