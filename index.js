b429710db6f365ddb22998b1750e1ecaea565319
Sergei Maksimov.com <s.mksimov@gmail.com>
2021-10-23 21:51:52 +0300

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 5000;
-
+const DB_URL = 'mongodb+srv://user:user@cluster0.ivmut.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const app = express();
 
 app.use(express.json())
@@ -10,6 +11,13 @@ app.post('/', (req, res) => {
   res.status(200).json('Server online!');
 });
 
-app.listen(PORT, () => {
-  console.log('you server is started on port' + PORT)
-});
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => { console.log('you server is started on port' + PORT) });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startApp();
